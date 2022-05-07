337ba8bb61152cc7e3ab775a41bfd2666df68b27
Ezra Muir <ezramuir12@gmail.com>
2022-05-07 17:32:46 -0500

    deleted files not needed

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const Main = () => {
+
+    // 8d30802d8432a8a64efed4160fed7791
+    const [movies, setMovies] = useState([])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Main
