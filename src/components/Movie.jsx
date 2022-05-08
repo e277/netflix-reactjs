4e9378b3162a9542856e11379d81b19fb3dd321b
Ezra Muir <ezramuir12@gmail.com>
2022-05-08 18:03:07 -0500

    save user liked shows to firebase

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,10 +3,34 @@ import {
     FaHeart,
     FaRegHeart
 } from 'react-icons/fa'
+import { UserAuth } from '../context/AuthContext'
+import { db } from '../firebase'
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore'
+import { async } from '@firebase/util'
 
 const Movie = ({ movieItem }) => {
 
     const [likes, setLikes] = useState(false)
+    const [saved, setSaved] = useState(false)
+    const { user } = UserAuth()
+
+    const movieID = doc(db, 'users', `${user?.email}`)
+
+    const saveShow = async () => {
+        if (user?.email) {
+            setLikes(!likes)
+            setSaved(!saved)
+            await updateDoc(movieID, {
+                saveShows: arrayUnion({
+                    id: movieItem.id,
+                    title: movieItem.title,
+                    img: movieItem.backdrop_path
+                })
+            })
+        } else {
+            alert(`Please Login to save a movie!!!`)
+        }
+    }
 
     return (
         <>
@@ -14,7 +38,7 @@ const Movie = ({ movieItem }) => {
                 <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${movieItem?.backdrop_path}`} alt={movieItem?.title} />
                 <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
                     <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{movieItem?.title}</p>
-                    <p className=''>{likes ? <FaHeart className='absolute top-4 left-4 text-red-500' /> : <FaRegHeart className='absolute top-4 left-4 text-slate-400' />}</p>
+                    <p onClick={saveShow}>{likes ? <FaHeart className='absolute top-4 left-4 text-red-500' /> : <FaRegHeart className='absolute top-4 left-4 text-slate-400' />}</p>
                 </div>
             </div>
         </>
