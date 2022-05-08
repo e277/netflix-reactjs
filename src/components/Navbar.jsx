76f0ee748c983b4a8ef3e8814030e937af8ba60a
Ezra Muir <ezramuir12@gmail.com>
2022-05-08 09:23:54 -0500

    display each section in a row

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
             <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>
-                EzraFLIX</h1>
+                UngguFLIX</h1>
             <div>
                 <button className='text-white pr-4'>
                     Sign In</button>
